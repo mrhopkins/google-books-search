a35bcb18950d301e06f74cc3137c36fe7d915768
Matthew Hopkins <mattrhopkins@gmail.com>
2019-05-03 11:35:33 -0600

    changed navbar from fixed to static

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,22 +2,22 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-            <a className="navbar-brand text-white" href="/">Google Books</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-primary static-top">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+                <a className="navbar-brand" href="/">Google Books</a>
+                <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="/">Search</a>
+                        <a className="nav-link" href="/">Search</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white" href="/saved">Saved</a>
+                        <a className="nav-link" href="/saved">Saved</a>
                     </li>
                 </ul>
-            </div>
+            </nav>
         </nav>
     )
         };
