1c249bc297cfaa4361156c6bc20ec19cece12e49
Matthew Hopkins <mattrhopkins@gmail.com>
2019-05-03 00:43:59 -0600

    prod build #1

---
+++ client/build/precache-manifest.9e0b5ae67b6d920bcb92aa554ffbebcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2269c9032625f0fed74",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "b2269c9032625f0fed74",
+    "url": "/static/js/main.e9cc030a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "417cb6d89b04eaff7faa",
+    "url": "/static/js/2.f5ee2e6d.chunk.js"
+  },
+  {
+    "revision": "af80487ddb18ff5d58030d56add3e2d6",
+    "url": "/index.html"
+  }
+];
